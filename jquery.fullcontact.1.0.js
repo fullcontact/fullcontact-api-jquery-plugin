87544c183080c417ce70f9c51278076178ff1f5b
Matt Elliott <mattdelliott@me.com>
2011-12-09 14:49:45 -0700

    Initial Commit of FullContact jQuery Plugin

---
+++ jquery.fullcontact.1.0.js
@@ -0,0 +1,40 @@
+//jQuery FullContact Plugin - VERSION 1.0
+
+(function($) {
+	$.fullcontact = {
+		baseURL: 'https://api.fullcontact.com/',
+		vcardEndpoint: 'v2/person.vcf',
+		personLookupEndpoint: 'v1/person.json',
+		buildURL: function(endpoint, apiKey, options) {
+			var optionsString = ""
+			for(var opt in options) optionsString += "&"+escape(opt)+"="+escape(options[opt]);
+			optionsString = "?apiKey="+apiKey+optionsString;
+			return this.baseURL+endpoint+optionsString;
+		},
+		executeRequest: function(url, method, dataType, oncomplete, data) {
+			$.ajax(url,{
+                                async:true,
+				type: method,
+                                success: function(response){
+                                        if(oncomplete) oncomplete(response);
+                                },
+                                error: function(obj,status,error){
+                                        if(oncomplete) oncomplete({status: obj.status, message: error});
+                                },
+				data: data,
+                                dataType: dataType 
+                        });
+		},
+		emailLookup: function(apiKey,emailAddress,oncomplete,options) {
+			options = options || {};
+			options.email = emailAddress;
+			var url = this.buildURL(this.personLookupEndpoint,apiKey,options);
+			this.executeRequest(url,'GET','json',oncomplete);
+		},
+		enrichVCard: function(apiKey,vcard,oncomplete,options) {
+			var url = this.buildURL(this.vcardEndpoint,apiKey,options);
+			this.executeRequest(url,'POST','text',oncomplete,{vcard:vcard});
+		} 
+  	};
+	$.fn.fullcontact = $.fullcontact;
+})(jQuery);
